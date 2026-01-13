2ddc06c1d1d139246e94b12cfd8f4a5a634901ba
Andre Alves <andresistinfo@gmail.com>
2026-01-13 07:13:19 +0000

    Renaming build app

--- src/app/components/docs/docs.ts
+++ src/app/components/docs/docs.ts
@@ -2,14 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-    selector: 'app-docs',
-    standalone: true,
-    imports: [RouterOutlet],
-    template: `<div class="docs-container container"><router-outlet></router-outlet></div>`,
-    styles: [`
+  selector: 'app-docs',
+  standalone: true,
+  imports: [RouterOutlet],
+  template: `<div class="docs-container container"><router-outlet></router-outlet></div>`,
+  styles: [`
     .docs-container {
-      padding: 0 20px;
-      margin-top: 2rem;
+      padding: 100px 20px 0 20px;
     }
   `]
 })
